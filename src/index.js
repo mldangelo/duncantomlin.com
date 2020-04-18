1ef8cc2a6b2e9f02e8c4307ccfe6e4d2fbb0dd0b
mldangelo <michael.l.dangelo@gmail.com>
2020-04-18 18:46:37 -0400

    added linter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {hydrate, render} from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.querySelector('#root');
+
+if (rootElement.hasChildNodes()) {
+	hydrate(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>, rootElement);
+} else {
+	render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>,
+		rootElement);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
