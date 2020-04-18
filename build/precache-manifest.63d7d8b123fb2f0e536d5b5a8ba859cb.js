2d7179f17c2c752fc253f4d9f4f8f230fc6027f6
mldangelo <michael.l.dangelo@gmail.com>
2020-04-18 18:47:22 -0400

    added build directory

---
+++ build/precache-manifest.63d7d8b123fb2f0e536d5b5a8ba859cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28bcd248c6274f2eb167a5414aeb4c95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adbecc42554039aeca56",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f359ee7aee308db69110",
+    "url": "/static/js/2.8c2c4a0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8c2c4a0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adbecc42554039aeca56",
+    "url": "/static/js/main.68ec3cd4.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
