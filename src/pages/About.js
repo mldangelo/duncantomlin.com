2b4c8b65cdce9be15c420b1d2448ac9161587873
mldangelo <michael.l.dangelo@gmail.com>
2020-04-19 17:22:45 -0400

    added about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Iframe from 'react-iframe'
 
 const About = () => (
-	<div>
-	</div>
+    <>
+        <Iframe url="/about.html"
+            width="100%"
+            height="1000px"
+            display="initial"
+            position="relative"/>
+        <h1>END</h1>
+    </>
 );
 
 export default About;
