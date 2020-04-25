bb3db313a958c9dfa448112ff7b26305acf3ae23
mldangelo <michael.l.dangelo@gmail.com>
2020-04-25 18:14:45 -0400

    added audio

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import ReactSound from 'react-sound';
 import logo from '../static/gifs/static.gif';
+
+const Sound = ReactSound.default;
 
 const NotFound = () => (
   <>
+    <ReactSound 
+        url="/static.m4a"
+        autoLoad
+        playStatus="PLAYING"
+        loop
+        playbackRate={1}
+        playFromPosition={500}
+    />
     <img src={logo} className="static" alt="static" />
   </>
 );
