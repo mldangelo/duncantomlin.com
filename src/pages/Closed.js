5c37e1124bbc1b895ab0426d493a539679931956
mldangelo <michael.l.dangelo@gmail.com>
2020-04-25 17:54:10 -0400

    only show closed page when closed

--- src/pages/Closed.js
+++ src/pages/Closed.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
-const About = () => (
+import isOpen from '../utils/isOpen';
+
+const About = () => (!isOpen() ? (
   <>
     <h1>We are closed. Please come back later</h1>
     <h1>We are closed. Please come back later</h1>
@@ -26,6 +29,6 @@ const About = () => (
     <h1>We are closed. Please come back later</h1>
     <h6>Normal website hours are 9 AM - 5 PM ET M-F</h6>
   </>
-);
+) : <Redirect to="/"/>);
 
 export default About;
