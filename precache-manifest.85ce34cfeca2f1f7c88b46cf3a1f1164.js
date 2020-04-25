2f3be9a6e8620e922e7bc4e269790d696f61f179
mldangelo <michael.l.dangelo@gmail.com>
2020-04-25 18:38:21 -0400

    Updates

---
+++ precache-manifest.85ce34cfeca2f1f7c88b46cf3a1f1164.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e863defa975a00745b202c937941951",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a08fcd51c35729d252c5",
+    "url": "/static/css/main.fa4ffe60.chunk.css"
+  },
+  {
+    "revision": "ed9c77708a1ff6b633c0",
+    "url": "/static/js/2.1cd584ff.chunk.js"
+  },
+  {
+    "revision": "e420ff54aaee9ec65dfee3e735742ec2",
+    "url": "/static/js/2.1cd584ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a08fcd51c35729d252c5",
+    "url": "/static/js/main.aac155c2.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  },
+  {
+    "revision": "9237db2e2da61136bbadf3e18390e396",
+    "url": "/static/media/skateboard.9237db2e.gif"
+  },
+  {
+    "revision": "e171b118e48e278b35026f5966d7a6ff",
+    "url": "/static/media/static.e171b118.gif"
+  }
+]);
