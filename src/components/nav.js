84e92bebbfbc87825887571317ae0a93731fce6f
mldangelo <michael.l.dangelo@gmail.com>
2020-05-10 21:09:51 -0400

    update navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-const Nav = () => (
-  <nav className="navbar" role="navigation">
-    <Link to="/" className="navbar-item">Home</Link>
-    <Link to="/about" className="navbar-item">About</Link>
-    <Link to="/criticism" className="navbar-item">Criticism</Link>
-    <Link to="/curation" className="navbar-item">Curation</Link>
-    <Link to="/creative-content" className="navbar-item">Creative Content</Link>
-    <Link to="/projects" className="navbar-item">Projects</Link>
-    <Link to="/contact" className="navbar-item">Contact</Link>
-  </nav>
-);
+const Nav = () => {
+  
+  const { pathname } = useLocation();
+
+  const links = [
+    { path: '/', title: 'Home'},
+    { path: '/about', title: 'About'},
+    { path: '/criticism', title: 'Criticism'},
+    { path: '/curation', title: 'Curation'},
+    { path: '/creative-content', title: 'Creative Content'},
+    { path: '/projects', title: 'Projects'},
+    { path: '/contact', title: 'Contact'},
+  ]
+
+  return (
+    <nav className="navbar" role="navigation">
+      {links.map(({path, title}) => (
+        <Link to={path} className={`navbar-item ${path == pathname ? 'is-active' : null}`}>{title}</Link>
+      ))}
+    </nav>
+  );
+};
 
 export default Nav;
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,21 +5,10 @@ import logo from '../static/gifs/skateboard.gif';
 
 const Index = () => (
   <Template>
-    <div className="columns">
-      <header className="">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div class="column">
-        <p>
-          Welcome to
-          {' '}
-          <a href="/">
-            Duncan Tomlin
-            <code>.com</code>
-          </a>
-        </p>
-        </div>
-      </header>
-    </div>
+    {/* <img src={logo} className="skateboard" style={{height:'500px !important'}}/> */}
+    <section class="section">
+      <h1 className="title is-3">Welcome to{' '}<a href="/">Duncan Tomlin<code>.com</code></a></h1>
+    </section>
   </Template>
 );
 
