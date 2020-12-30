58a9710e339b6ae3034f781a57d6449eb765e2ab
mldangelo <michael.l.dangelo@gmail.com>
2020-12-30 14:06:21 -0500

    use hooks to set index routes

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,37 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Template from '../components/template';
 
-const Index = () => (
-  <Template>
-    <section className="hero is-fullheight-with-navbar">
-      <div className="hero-body">
-        <div className="container">
-          <h1 className="title">
-            In what context do you know Duncan?
-          </h1>
-          <div className="field has-addons">
-            <div className="control">
-              <div className="select is-medium is-expanded">
-                <select name="context">
-                  <option>I don&apos;t</option>
-                  <option>We&apos;re dear friends</option>
-                  <option>
-                    I am an esteemed member of the admissions office at Columbia GSAPP{' '}
-                  </option>
-                  <option>We will come to know each other as distribution partners</option>
-                  <option>As his reputation precedes him</option>
-                  <option>romantic</option>
-                </select>
+const options = [
+  {
+    label: 'I don\'t',
+    href: '/about',
+  },
+  {
+    label: 'We\'re dear friends',
+    href: '/posts',
+  },
+  {
+    label: 'I am an esteemed member of the admissions committee at Columbia GSAPP',
+    href: '/columbia-m-arch-application-materials',
+  },
+  {
+    label: 'We will come to know each other as distribution partners',
+    href: '/minetta-mizzenmast-and-west-12th-indies-nyc',
+  },
+  {
+    label: 'As his reputation precedes him',
+    href: '/professional-work',
+  },
+  {
+    label: 'Romantic',
+    href: '/maggie',
+  },
+];
+
+const Index = () => {
+  const history = useHistory();
+
+  const [value, setSelected] = useState({ label: 'home', href: '/' }); // holds url
+
+  const handleClick = () => {
+    history.push(value?.href || '/');
+  };
+
+  return (
+    <Template>
+      <section className="hero is-fullheight-with-navbar">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">
+              In what context do you know Duncan?
+            </h1>
+            <div className="field has-addons">
+              <div className="control">
+                <div className="select is-medium is-expanded">
+                  <select name="context" onChange={(e) => setSelected(options.find(({ label }) => label === e.target.value))}>
+                    {options.map(({ label }) => (<option key={label}>{label}</option>))}
+                  </select>
+                </div>
               </div>
-            </div>
-            <div className="control">
-              <button type="button" className="button is-link is-medium">Go</button>
+              <div className="control">
+                <button type="button" className="button is-link is-medium" onClick={() => handleClick()}>Go</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  </Template>
-);
+      </section>
+    </Template>
+  );
+};
 
 export default Index;
