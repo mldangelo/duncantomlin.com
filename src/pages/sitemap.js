415cd4ba8e17964744c12f8af5b69e4cf7b237a5
mldangelo <michael.l.dangelo@gmail.com>
2020-12-31 16:01:33 -0500

    update sitemap to support more links

--- src/pages/sitemap.js
+++ src/pages/sitemap.js
@@ -18,12 +18,19 @@ const StaticPage = ({ paths }) => (
 
 // This also gets called at build time
 export async function getStaticProps() {
-  const files = glob.sync(path.join(process.cwd(), 'content/**/*.md'));
-  // less stable than regex. regex was not working for every route. fix later
-  const matches = files
+  const markdownMatches = glob
+    .sync(path.join(process.cwd(), 'content/**/*.md'))
     .map((file) => file.replace('.md', '').split('content/')[1])
     .filter((match) => !match.includes('README'));
 
+  const jsxMatches = glob
+    .sync(path.join(process.cwd(), 'src/pages/**/*.js'))
+    .map((file) => file.replace('.js', '').split('src/pages/')[1])
+    .filter(
+      (match) => match[0] !== '_' && match[0] !== '[' && match !== 'index'
+    );
+
+  const matches = [...markdownMatches, ...jsxMatches, '/'].sort();
   // Pass post data to the page via props
   return {
     props: { paths: matches },
